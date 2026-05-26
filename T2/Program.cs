a201596896a16e9a30a6b1f1e85f99d3b9586189
Lean <N00502100@upn.pe>
2026-05-26 01:22:35 -0500

    V2_Validacion

--- T2/Program.cs
+++ T2/Program.cs
@@ -9,12 +9,27 @@ class Program
 
         int[] notas = new int[cantidad];
 
+        // Ingreso y validación
         for (int i = 0; i < cantidad; i++)
         {
-            Console.Write($"Ingresa la nota {i + 1}: ");
-            notas[i] = int.Parse(Console.ReadLine());
+            int nota;
+
+            do
+            {
+                Console.Write($"\nIngresa la nota {i + 1}: ");
+                nota = int.Parse(Console.ReadLine());
+
+                if (nota < 0 || nota > 20)
+                {
+                    Console.WriteLine("Nota inválida, ingresa entre 0 y 20.");
+                }
+
+            } while (nota < 0 || nota > 20);
+
+            notas[i] = nota;
         }
 
+        // Mostrar notas válidas
         Console.WriteLine("\nNotas ingresadas:");
 
         for (int i = 0; i < cantidad; i++)
