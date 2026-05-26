de1615fd5566808d5f5121d1beffebdd6e07acbe
Lean <N00502100@upn.pe>
2026-05-26 01:44:43 -0500

    V3_Funciones

--- T2/Program.cs
+++ T2/Program.cs
@@ -1,42 +1,80 @@
 ﻿using System;
-
 class Program
 {
+    static void CalcularDatos(int[] notas, int cantidad, ref double promedio, ref int maxima, ref int minima)
+    {
+        int suma=0;
+        maxima=notas[0];
+        minima=notas[0];
+
+        for(int i=0; i<cantidad;i++)
+        {
+        suma+=notas[i];
+
+            if (notas[i]>maxima)
+            {
+            maxima=notas[i];
+            }
+
+            if (notas[i]<minima)
+            {
+            minima=notas[i];
+            }
+        }
+
+        promedio =(double)suma/cantidad;
+    }
+    static int ContarAprobados(int[]notas,int cantidad)
+    {
+        int aprobados=0;
+        for (int i=0;i<cantidad;i++)
+        {
+            if (notas[i]>=12)
+            {
+            aprobados++;
+            }
+        }
+        return aprobados;
+    }
     static void Main(string[] args)
     {
         Console.Write("¿Cuántas notas deseas ingresar? ");
         int cantidad = int.Parse(Console.ReadLine());
-
-        int[] notas = new int[cantidad];
-
-        // Ingreso y validación
+        int[] notas=new int[cantidad];
         for (int i = 0; i < cantidad; i++)
         {
             int nota;
-
             do
             {
                 Console.Write($"\nIngresa la nota {i + 1}: ");
                 nota = int.Parse(Console.ReadLine());
-
                 if (nota < 0 || nota > 20)
                 {
                     Console.WriteLine("Nota inválida, ingresa entre 0 y 20.");
                 }
-
             } while (nota < 0 || nota > 20);
 
             notas[i] = nota;
         }
-
-        // Mostrar notas válidas
-        Console.WriteLine("\nNotas ingresadas:");
-
+        double promedio = 0;
+        int maxima = 0;
+        int minima = 0;
+        CalcularDatos(notas, cantidad, ref promedio, ref maxima, ref minima);
+        int aprobados = ContarAprobados(notas, cantidad);
+        Console.WriteLine("\n--- Datos Calculados ---");
+        Console.Write("Notas ingresadas: [");
         for (int i = 0; i < cantidad; i++)
         {
-            Console.WriteLine(notas[i]);
+        Console.Write(notas[i]);
+        if (i < cantidad - 1)
+            {
+        Console.Write(", ");
+            }
         }
-
-        Console.ReadKey();
+        Console.WriteLine("]");
+        Console.WriteLine($"\nPromedio      : {promedio:F2}");
+        Console.WriteLine($"Nota máxima   : {maxima}");
+        Console.WriteLine($"Nota mínima   : {minima}");
+        Console.WriteLine($"Aprobados     : {aprobados}");
     }
 }
