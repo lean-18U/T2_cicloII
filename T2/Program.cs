07da61e2ff336c4632956271fa59dea07b0200f3
Lean <N00502100@upn.pe>
2026-05-26 01:15:29 -0500

    V1_Estructura

---
+++ T2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("¿Cuántas notas deseas ingresar? ");
+        int cantidad = int.Parse(Console.ReadLine());
+
+        int[] notas = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write($"Ingresa la nota {i + 1}: ");
+            notas[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nNotas ingresadas:");
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.WriteLine(notas[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
